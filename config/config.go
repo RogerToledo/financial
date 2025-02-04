4413fc8e4420b3f7ec2edd2c8cf8db6f49d41173
Roger Toledo <rogertoledo28@gmail.com>
2025-02-04 17:26:59 -0300

    feat: add configs

---
+++ config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import "github.com/spf13/viper"
+
+type config struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+type APIConfig struct {
+	Port string
+}
+
+type DBConfig struct {
+	StringConn string
+}
+
+var cfg *config
+
+func Load() error {
+	viper.SetConfigName("cfg")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("./config")
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	cfg = new(config)
+
+	cfg.API = APIConfig{
+		Port: viper.GetString("api.port"),
+	}
+
+	cfg.DB = DBConfig{
+		StringConn: viper.GetString("db.stringConn"),
+	}
+
+	return nil
+}
+
+func DB() DBConfig {
+	return cfg.DB
+}
+
+func ServerPort() string {
+	return cfg.API.Port
+}
