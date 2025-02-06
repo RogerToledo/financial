b62311610ee064edf0c397a3774893866756bc86
Roger Toledo <rogertoledo28@gmail.com>
2025-02-06 08:44:22 -0300

    feat: add router files

---
+++ router/person.go
@@ -0,0 +1,32 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/me/financial/repository"
+
+	"github.com/me/financial/handlers"
+)
+
+func PersonRoutes(mux *http.ServeMux, rep *repository.Repository) {
+	
+	mux.HandleFunc("POST /person", func(w http.ResponseWriter, r *http.Request) {
+		handlers.CreatePerson(rep, w, r)
+	})
+	
+	mux.HandleFunc("PUT /person/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.UpdatePerson(rep, w, r)
+	})
+
+	mux.HandleFunc("DELETE /person/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.DeletePerson(rep, w, r)
+	})
+
+	mux.HandleFunc("GET /person/{name}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.FindPersonByName(rep, w, r)
+	})
+
+	mux.HandleFunc("GET /persons", func(w http.ResponseWriter, r *http.Request) {
+		handlers.FindAllPersons(rep, w, r)
+	})
+}
---
+++ router/router.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/me/financial/config"
+	"github.com/me/financial/db"
+	"github.com/me/financial/repository"
+)
+
+func InitializeRoutes() {
+	db, err := db.NewDB()
+	if err != nil {
+		panic(err)
+	}
+
+	rep := repository.NewRepository(db)
+
+	mux := http.NewServeMux()
+	
+	PersonRoutes(mux, rep)
+	CreditCardRoutes(mux, rep)
+
+	log.Printf("Server running on port %s", config.ServerPort())
+	http.ListenAndServe(fmt.Sprintf(":%s", config.ServerPort()), mux)
+}
