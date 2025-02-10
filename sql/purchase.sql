eae572c2a0614bc92ece12ea15c46e9f16012f54
Roger Toledo <rogertoledo28@gmail.com>
2025-02-09 22:01:23 -0300

    chore: update purchase.sql

--- sql/purchase.sql
+++ sql/purchase.sql
@@ -12,17 +12,17 @@ CREATE TABLE financial.purchase (
 	installment_number int4 DEFAULT 0 NULL,
 	installment numeric(10, 2) DEFAULT 0 NULL,
 	place varchar(60) NULL,
-	id_payment_type int4 NOT NULL,
-	id_purchase_type int4 NOT NULL,
-	id_credit_card int4 NOT NULL,
-	id_person int4 NOT NULL,
+	id_payment_type uuid NOT NULL,
+	id_purchase_type uuid NOT NULL,
+	id_credit_card uuid NOT NULL,
+	id_person uuid NOT NULL,
 	CONSTRAINT purchase_pk PRIMARY KEY (id)
 );
 
 
 -- financial.purchase foreign keys
 
-ALTER TABLE financial.purchase ADD CONSTRAINT purchase_person_fk FOREIGN KEY (id) REFERENCES financial.person(id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE financial.purchase ADD CONSTRAINT purchase_credit_card_fk FOREIGN KEY (id) REFERENCES financial.credit_card(id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE financial.purchase ADD CONSTRAINT purchase_payment_type_fk FOREIGN KEY (id) REFERENCES financial.payment_type(id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE financial.purchase ADD CONSTRAINT purchase_purchase_type_fk FOREIGN KEY (id) REFERENCES financial.purchase_type(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE financial.purchase ADD CONSTRAINT purchase_credit_card_fk FOREIGN KEY (id_credit_card) REFERENCES financial.credit_card(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE financial.purchase ADD CONSTRAINT purchase_payment_type_fk FOREIGN KEY (id_payment_type) REFERENCES financial.payment_type(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE financial.purchase ADD CONSTRAINT purchase_person_fk FOREIGN KEY (id_person) REFERENCES financial.person(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE financial.purchase ADD CONSTRAINT purchase_purchase_type_fk FOREIGN KEY (id_purchase_type) REFERENCES financial.purchase_type(id) ON DELETE CASCADE ON UPDATE CASCADE;
